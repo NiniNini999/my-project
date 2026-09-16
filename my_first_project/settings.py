eb5d3ceeb30d4198e58695a9bb0990cd130fb747
Mariam <nchampuridze3@gmail.com>
2026-09-17 00:10:16 +0400

    verity theme

--- my_first_project/settings.py
+++ my_first_project/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
